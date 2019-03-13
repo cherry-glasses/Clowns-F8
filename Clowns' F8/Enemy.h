4c0177e2b23abfeee7a12cb923d455662df12e3f
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-03-13 23:03:00 +0100

    preparing bulldog class

--- Clowns' F8/Enemy.h
+++ Clowns' F8/Enemy.h
@@ -1,34 +1,34 @@
-//#ifndef __Enemy_H__
-//#define __Enemy_H__
-//
-//#include "Animation.h"
-//#include "Module.h"
-//#include "Entity.h"
-//
-//class Enemy : public Entity
-//{
-//public:
-//	Enemy(ENTITY_TYPE type);
-//
-//	// Destructor
-//	virtual ~Enemy();
-//
-//	// Called before the first frame
-//	virtual bool Start();
-//
-//	// Called each loop iteration
-//	virtual bool PreUpdate();
-//	virtual bool Update(float dt);
-//	virtual bool PostUpdate();
-//
-//	// Called before quitting
-//	virtual bool CleanUp();
-//
-//	//Save and Load
-//	virtual bool Load(pugi::xml_node&);
-//	virtual bool Save(pugi::xml_node&) const;
-//
-//
-//};
-//
-//#endif // __ENEMY_H__
+#ifndef __Enemy_H__
+#define __Enemy_H__
+
+#include "Animation.h"
+#include "Module.h"
+#include "Entity.h"
+
+class Enemy : public Entity
+{
+public:
+	Enemy(ENTITY_TYPE type);
+
+	// Destructor
+	virtual ~Enemy();
+
+	// Called before the first frame
+	virtual bool Start();
+
+	// Called each loop iteration
+	virtual bool PreUpdate();
+	virtual bool Update(float dt);
+	virtual bool PostUpdate();
+
+	// Called before quitting
+	virtual bool CleanUp();
+
+	//Save and Load
+	virtual bool Load(pugi::xml_node&);
+	virtual bool Save(pugi::xml_node&) const;
+
+
+};
+
+#endif // __ENEMY_H__
