d4e808c5c9fa3b6cc08d0a05f44ffbe56655a81c
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-03-20 19:39:18 +0100

    module pathfinding done

--- Clowns' F8/Application.cpp
+++ Clowns' F8/Application.cpp
@@ -7,6 +7,7 @@
 #include "ModuleAudio.h"
 #include "ModuleFonts.h"
 #include "ModuleMap.h"
+#include "ModulePathfinding.h"
 #include "ModuleFadeToBlack.h"
 #include "Scene.h"
 #include "ModuleEntityManager.h"
@@ -25,6 +26,7 @@ Application::Application(int _argc, char* _args[]) : argc(argc), args(args)
 	audio = new ModuleAudio();
 	fonts = new ModuleFonts();
 	map = new ModuleMap();
+	pathfinding = new ModulePathfinding();
 	fade_to_black = new ModuleFadeToBlack();
 	scene = new Scene();
 	entity_manager = new ModuleEntityManager();
@@ -39,6 +41,7 @@ Application::Application(int _argc, char* _args[]) : argc(argc), args(args)
 	AddModule(audio);
 	AddModule(fonts);
 	AddModule(map);
+	AddModule(pathfinding);
 	AddModule(fade_to_black);
 	AddModule(scene);
 	AddModule(entity_manager);
