237f05b437db2898946611c2eff5b019e11d0b6b
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-04-29 19:53:15 +0200

    Update Color.cpp

--- Clowns' F8/Color.cpp
+++ Clowns' F8/Color.cpp
@@ -9,4 +9,4 @@ Color Cian = Color(0.0F, 255.0F, 255.0F);
 Color Yellow = Color(255.0F, 255.0F, 0.0F);
 Color Pink = Color(255.0F, 155.0F, 255.0F);
 Color DarkGray = Color(75.0F, 75.0F, 100.0F);
-Color Scene_1_color = Color(75.0F, 75.0F, 200.0F);
+Color Scene_1_color = Color(194.0F, 213.0F, 231.0F);
