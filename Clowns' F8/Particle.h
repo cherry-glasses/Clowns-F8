bd3a1999a3107cc40cd35ee907ebffd3627279d3
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-05-23 14:58:30 +0200

    particle system prepared

--- Clowns' F8/Particle.h
+++ Clowns' F8/Particle.h
@@ -1,92 +1,92 @@
-//#ifndef __PARTICLE_H__
-//#define __PARTICLE_H__
-//
-//#include "Application.h"
-//#include "SDL/include/SDL.h"
-//
-//#define MIN_LIFE_TO_INTERPOLATE 15
-//
-//struct SDL_Texture;
-//
-//class Particle
-//{
-//private:
-//
-//	/*  This is the only variable we care about no matter if
-//	   the particle is alive or dead */
-//	uint life = 0;
-//
-//	struct Vortex
-//	{
-//		std::pair<float, float> pos = { 0.0f, 0.0f };
-//		float speed = 0.0f;
-//		float scale = 0.0f;
-//	} vortex;
-//
-//	union ParticleInfo
-//	{
-//		/* This struct holds the state of the particle when
-//		   it's being update (it's still alive).*/
-//		struct ParticleState
-//		{
-//			uint startLife;
-//			std::pair<float, float> pos;
-//			std::pair<float, float> startVel;
-//			std::pair<float, float> endVel;
-//			std::pair<float, float> currentVel;
-//			float currentSize, startSize, endSize;
-//			float ageRatio;
-//			float angle;
-//			double startRotSpeed;
-//			double currentRotSpeed;
-//			SDL_Rect pRect;
-//			SDL_Rect rectSize;
-//			SDL_Color startColor;
-//			SDL_Color endColor;
-//			SDL_BlendMode blendMode;
-//			bool vortexSensitive;
-//			float t;
-//
-//			ParticleState() {}
-//
-//		} pLive;
-//
-//		/* If the particle is dead, then the 'next' member comes
-//		   into play and the struct it's not used. This pointer
-//		   called 'next' holds a pointer to the next available
-//		   particle after this one. */
-//		Particle* next;
-//
-//		ParticleInfo() {}
-//	} pState;
-//
-//public:
-//
-//	Particle();
-//
-//	// Initializes new generated particle
-//	void Init(std::pair<float, float> pos, float startSpeed, float endSpeed, float angle, double rotSpeed, float startSize, float endSize, uint life, SDL_Rect textureRect, SDL_Color startColor, SDL_Color endColor, SDL_BlendMode blendMode, bool vortexSensitive);
-//
-//	// Generic methods
-//	void Update(float dt);
-//	bool Draw();
-//	bool IsAlive();
-//
-//	// Particle pointer methods
-//	Particle* GetNext();
-//	void SetNext(Particle* next);
-//
-//	// Given two colors interpolates linearly over time and returns the resulting color
-//	SDL_Color RgbInterpolation(SDL_Color startColor, float timeStep, SDL_Color endColor);
-//
-//	float InterpolateBetweenRange(float min, float timeStep, float max);
-//
-//	// Adds a vortex to the system
-//	void AddVortex(std::pair<float, float> pos, float speed, float scale);
-//
-//	/* Calculates particle position considering its velocity
-//	   and if there's a vortex in the system */
-//	void CalculateParticlePos(float dt);
-//};
-//
-//#endif
+#ifndef __PARTICLE_H__
+#define __PARTICLE_H__
+
+#include "Application.h"
+#include "SDL/include/SDL.h"
+
+#define MIN_LIFE_TO_INTERPOLATE 15
+
+struct SDL_Texture;
+
+class Particle
+{
+private:
+
+	/*  This is the only variable we care about no matter if
+	   the particle is alive or dead */
+	uint life = 0;
+
+	struct Vortex
+	{
+		std::pair<float, float> pos = { 0.0f, 0.0f };
+		float speed = 0.0f;
+		float scale = 0.0f;
+	} vortex;
+
+	union ParticleInfo
+	{
+		/* This struct holds the state of the particle when
+		   it's being update (it's still alive).*/
+		struct ParticleState
+		{
+			uint startLife;
+			std::pair<float, float> pos;
+			std::pair<float, float> startVel;
+			std::pair<float, float> endVel;
+			std::pair<float, float> currentVel;
+			float currentSize, startSize, endSize;
+			float ageRatio;
+			float angle;
+			double startRotSpeed;
+			double currentRotSpeed;
+			SDL_Rect pRect;
+			SDL_Rect rectSize;
+			SDL_Color startColor;
+			SDL_Color endColor;
+			SDL_BlendMode blendMode;
+			bool vortexSensitive;
+			float t;
+
+			ParticleState() {}
+
+		} pLive;
+
+		/* If the particle is dead, then the 'next' member comes
+		   into play and the struct it's not used. This pointer
+		   called 'next' holds a pointer to the next available
+		   particle after this one. */
+		Particle* next;
+
+		ParticleInfo() {}
+	} pState;
+
+public:
+
+	Particle();
+
+	// Initializes new generated particle
+	void Init(std::pair<float, float> pos, float startSpeed, float endSpeed, float angle, double rotSpeed, float startSize, float endSize, uint life, SDL_Rect textureRect, SDL_Color startColor, SDL_Color endColor, SDL_BlendMode blendMode, bool vortexSensitive);
+
+	// Generic methods
+	void Update(float dt);
+	bool Draw();
+	bool IsAlive();
+
+	// Particle pointer methods
+	Particle* GetNext();
+	void SetNext(Particle* next);
+
+	// Given two colors interpolates linearly over time and returns the resulting color
+	SDL_Color RgbInterpolation(SDL_Color startColor, float timeStep, SDL_Color endColor);
+
+	float InterpolateBetweenRange(float min, float timeStep, float max);
+
+	// Adds a vortex to the system
+	void AddVortex(std::pair<float, float> pos, float speed, float scale);
+
+	/* Calculates particle position considering its velocity
+	   and if there's a vortex in the system */
+	void CalculateParticlePos(float dt);
+};
+
+#endif
--- Clowns' F8/ParticlePool.h
+++ Clowns' F8/ParticlePool.h
@@ -1,43 +1,45 @@
-//#ifndef __PARTICLE_POOL_H__
-//#define __PARTICLE_POOL_H__
-//
-//#include "Particle.h"
-//#include "Emitter.h"
-////#include <vector>
-//
-//class Emitter;
-//
-//enum ParticleState
-//{
-//	PARTICLE_STATE_NOT_DEF,
-//	PARTICLE_ALIVE_DRAWN,
-//	PARTICLE_ALIVE_NOT_DRAWN,
-//	PARTICLE_DEAD
-//};
-//
-//class ParticlePool
-//{
-//private:
-//
-//	int poolSize = 0;
-//	static Particle* firstAvailable;
-//	Particle* particleArray = nullptr;
-//
-//public:
-//
-//	ParticlePool(Emitter* emitter);
-//	virtual ~ParticlePool();
-//
-//	// Generates a new particle each time it's called
-//	void Generate(std::pair<float, float> pos, float startSpeed, float endSpeed, float angle, float rotSpeed, float startSize, float endSize, uint life, SDL_Rect textureRect, SDL_Color startColor, SDL_Color endColor, SDL_BlendMode blendMode, bool vortexSensitive);
-//
-//	// Update (move and draw) particles in the pool.
-//	// IMPORTANT NOTE: This method returns:
-//	//		- PARTICLE_ALIVE_DRAWN: if the particle is still alive and has been drawn succesfully
-//	//      - PARTICLE_ALIVE_NOT_DRAWN:  if the particle is alive and has NOT been drawn succesfully
-//	//		- PARTICLE_DEAD: if is particle is no longer alive
-//
-//	ParticleState Update(float dt);
-//};
-//
-//#endif
+#ifndef __PARTICLE_POOL_H__
+#define __PARTICLE_POOL_H__
+
+#include "Particle.h"
+#include "Emitter.h"
+//#include <vector>
+
+class Emitter;
+
+enum ParticleState
+{
+	PARTICLE_STATE_NOT_DEF,
+	PARTICLE_ALIVE_DRAWN,
+	PARTICLE_ALIVE_NOT_DRAWN,
+	PARTICLE_DEAD
+};
+
+class ParticlePool
+{
+private:
+
+	int poolSize = 0;
+	Particle* firstAvailable;
+	Particle* particleArray = nullptr;
+
+public:
+
+	ParticlePool(Emitter* emitter);
+	virtual ~ParticlePool();
+
+	// Generates a new particle each time it's called
+	void Generate(std::pair<float, float> pos, float startSpeed, float endSpeed, float angle, float rotSpeed, float startSize, float endSize, uint life, SDL_Rect textureRect, SDL_Color startColor, SDL_Color endColor, SDL_BlendMode blendMode, bool vortexSensitive);
+
+	// Update (move and draw) particles in the pool.
+	// IMPORTANT NOTE: This method returns:
+	//		- PARTICLE_ALIVE_DRAWN: if the particle is still alive and has been drawn succesfully
+	//      - PARTICLE_ALIVE_NOT_DRAWN:  if the particle is alive and has NOT been drawn succesfully
+	//		- PARTICLE_DEAD: if is particle is no longer alive
+
+	ParticleState Update(float dt);
+
+	
+};
+
+#endif
