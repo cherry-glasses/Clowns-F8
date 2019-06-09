b651dc86cc235206e4ec955fe05b3ee8fd9c4ac6
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-06-09 18:26:43 +0200

    inputs

--- Clowns' F8/MainMenu.cpp
+++ Clowns' F8/MainMenu.cpp
@@ -110,25 +110,22 @@ bool MainMenu::Update(float _dt)
 			CreateControlsMenu();
 		}
 		if (accept_button->has_been_clicked) {
-			ChangeControls(App->input->keyboard_buttons.buttons_to_use.ACCEPT, accept_button);
+			ChangeControls(App->input->keyboard_buttons.buttons_to_use.ACCEPT, accept_button, true, accept_label3);
 		}
 		if (decline_button->has_been_clicked) {
-			ChangeControls(App->input->keyboard_buttons.buttons_to_use.DECLINE, decline_button);
-		}
-		if (character_stats_button->has_been_clicked) {
-			ChangeControls(App->input->keyboard_buttons.buttons_to_use.STATS, character_stats_button);
-		}
-		if (characeter_abilities_button->has_been_clicked) {
-			ChangeControls(App->input->keyboard_buttons.buttons_to_use.CHARACABILITY, characeter_abilities_button);
-		}
-		if (abilities_button->has_been_clicked) {
-			ChangeControls(App->input->keyboard_buttons.buttons_to_use.ABILITY, abilities_button);
+			ChangeControls(App->input->keyboard_buttons.buttons_to_use.DECLINE, decline_button, true, decline_label3);
 		}
 		if (start_button->has_been_clicked) {
-			ChangeControls(App->input->keyboard_buttons.buttons_to_use.PAUSE, start_button);
+			ChangeControls(App->input->keyboard_buttons.buttons_to_use.PAUSE, start_button, true, start_label3);
 		}
-		if (select_button->has_been_clicked) {
-			ChangeControls(App->input->keyboard_buttons.buttons_to_use.ABILITYTREE, select_button);
+		if (accept_button2->has_been_clicked) {
+			ChangeControls(App->input->keyboard_buttons.buttons_to_use.ACCEPT, accept_button2, false, accept_label4);
+		}
+		if (decline_button2->has_been_clicked) {
+			ChangeControls(App->input->keyboard_buttons.buttons_to_use.DECLINE, decline_button2, false, decline_label4);
+		}
+		if (start_button2->has_been_clicked) {
+			ChangeControls(App->input->keyboard_buttons.buttons_to_use.PAUSE, start_button2, false, start_label4);
 		}
 
 		else if (back_controls_button->has_been_clicked || App->input->Decline())
@@ -248,30 +245,45 @@ void MainMenu::CreateOptionMenu()
 void MainMenu::CreateControlsMenu()
 {
 	controls_menu_created = true;
-	accept_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), (screen_height / 2) - (option_background.h / 2) + (button.h * 0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
-	buttons.push_back(accept_button);
-	decline_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), accept_button->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
-	buttons.push_back(decline_button);
-	character_stats_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), decline_button->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
-	buttons.push_back(character_stats_button);
-	characeter_abilities_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), character_stats_button->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
-	buttons.push_back(characeter_abilities_button);
-	abilities_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), characeter_abilities_button->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
-	buttons.push_back(abilities_button);
-	start_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), abilities_button->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
-	buttons.push_back(start_button);
-	select_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), start_button->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
-	buttons.push_back(select_button);
-	back_controls_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), select_button->position.second + (button_margin + button.h * 1), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
-	buttons.push_back(back_controls_button);
 
-	accept_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, accept_button->position.first + (button.w / 2), accept_button->position.second + (button.h / 2), App->scene_manager->language->accept, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
-	decline_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, decline_button->position.first + (button.w / 2), decline_button->position.second + (button.h / 2), App->scene_manager->language->decline, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
-	character_stats_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, character_stats_button->position.first + (button.w / 2), character_stats_button->position.second + (button.h / 2), App->scene_manager->language->character_stats, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
-	character_abilites_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, characeter_abilities_button->position.first + (button.w / 2), characeter_abilities_button->position.second + (button.h / 2), App->scene_manager->language->character_abilities, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
-	abilities_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, abilities_button->position.first + (button.w / 2), abilities_button->position.second + (button.h / 2), App->scene_manager->language->abilities, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
-	start_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, start_button->position.first + (button.w / 2), start_button->position.second + (button.h / 2), App->scene_manager->language->pause, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
-	select_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, select_button->position.first + (button.w / 2), select_button->position.second + (button.h / 2), App->scene_manager->language->select, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	// Keyboard
+	accept_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 4), (screen_height / 2) - (option_background.h / 2) + (button.h * 1.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
+	buttons.push_back(accept_button);
+	decline_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 4), accept_button->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
+	buttons.push_back(decline_button);
+	start_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 4), decline_button->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
+	buttons.push_back(start_button);
+
+	control_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, (screen_width / 2), (screen_height / 2) - (option_background.h / 2) + (button.h), App->scene_manager->language->keyboard, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	accept_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, accept_button->position.first - (button.w / 5), accept_button->position.second + (button.h / 2), App->scene_manager->language->accept, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	decline_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, decline_button->position.first - (button.w / 5), decline_button->position.second + (button.h / 2), App->scene_manager->language->decline, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	start_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, start_button->position.first - (button.w / 5), start_button->position.second + (button.h / 2), App->scene_manager->language->pause, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	
+	accept_label3 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, accept_button->position.first + (button.w / 2), accept_button->position.second + (button.h / 2), SDL_GetScancodeName((SDL_Scancode)App->input->keyboard_buttons.buttons_to_use.ACCEPT), { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	decline_label3 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, decline_button->position.first + (button.w / 2), decline_button->position.second + (button.h / 2), SDL_GetScancodeName((SDL_Scancode)App->input->keyboard_buttons.buttons_to_use.DECLINE), { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	start_label3 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, start_button->position.first + (button.w / 2), start_button->position.second + (button.h / 2), SDL_GetScancodeName((SDL_Scancode)App->input->keyboard_buttons.buttons_to_use.PAUSE), { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+
+
+	// Gamepad
+	accept_button2 = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 4), start_button->position.second + (button_margin + button.h * 1.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
+	buttons.push_back(accept_button2);
+	decline_button2 = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 4), accept_button2->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
+	buttons.push_back(decline_button2);
+	start_button2 = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 4), decline_button2->position.second + (button_margin + button.h*0.5), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
+	buttons.push_back(start_button2);
+
+	control_label2 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, (screen_width / 2), start_button->position.second + (button_margin + button.h), App->scene_manager->language->gamepad, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	accept_label2 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, accept_button2->position.first - (button.w / 5), accept_button2->position.second + (button.h / 2), App->scene_manager->language->accept, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	decline_label2 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, decline_button2->position.first - (button.w / 5), decline_button2->position.second + (button.h / 2), App->scene_manager->language->decline, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	start_label2 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, start_button2->position.first - (button.w / 5), start_button2->position.second + (button.h / 2), App->scene_manager->language->pause, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	
+	accept_label4 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, accept_button2->position.first + (button.w / 2), accept_button2->position.second + (button.h / 2), SDL_GetScancodeName((SDL_Scancode)App->input->gamepad.A), { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	decline_label4 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, decline_button2->position.first + (button.w / 2), decline_button2->position.second + (button.h / 2), SDL_GetScancodeName((SDL_Scancode)App->input->gamepad.B), { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+	start_label4 = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, start_button2->position.first + (button.w / 2), start_button2->position.second + (button.h / 2), SDL_GetScancodeName((SDL_Scancode)App->input->gamepad.START), { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
+
+
+	back_controls_button = (GUIButton*)App->gui_manager->CreateGUIButton(GUI_ELEMENT_TYPE::GUI_BUTTON, (screen_width / 2) - (button.w / 2), start_button2->position.second + (button_margin + button.h * 1), { 0, 0, 288, 64 }, { 0, 64, 288, 64 }, { 0, 128, 288, 64 });
+	buttons.push_back(back_controls_button);
 	back_controls_label = (GUILabel*)App->gui_manager->CreateGUILabel(GUI_ELEMENT_TYPE::GUI_LABEL, back_controls_button->position.first + (button.w / 2), back_controls_button->position.second + (button.h / 2), App->scene_manager->language->back, { 0, 0, 0, 255 }, App->gui_manager->default_font_used);
 
 	accept_button->Select(SELECTED);
@@ -337,17 +349,53 @@ void MainMenu::ControlLanguageAndMusic()
 	}
 }
 
-void MainMenu::ChangeControls(int &control, GUIButton* button) {
-
-	if (isWaittingButt && App->input->scancode != SDL_SCANCODE_UNKNOWN && App->input->scancode != control && App->input->scancode != SDL_SCANCODE_ESCAPE)
+void MainMenu::ChangeControls(int &control, GUIButton* button, bool keyboard, GUILabel* label) {
+	
+	if (App->input->Decline())
 	{
-		control = App->input->scancode;
-		accept_button->has_been_clicked = false;
 		isWaittingButt = false;
-		
+		button->has_been_clicked = false;
+		button->Select(SELECTED);
 	}
-	else
-	isWaittingButt = true;
+	else if (keyboard)
+	{
+		if (isWaittingButt && App->input->scancode != SDL_SCANCODE_UNKNOWN
+			&& App->input->scancode != App->input->keyboard_buttons.buttons_to_use.ACCEPT
+			&& App->input->scancode != App->input->keyboard_buttons.buttons_to_use.DECLINE
+			&& App->input->scancode != App->input->keyboard_buttons.buttons_to_use.PAUSE
+			&& App->input->scancode != App->input->keyboard_buttons.buttons_to_use.UP
+			&& App->input->scancode != App->input->keyboard_buttons.buttons_to_use.DOWN
+			&& App->input->scancode != App->input->keyboard_buttons.buttons_to_use.LEFT
+			&& App->input->scancode != App->input->keyboard_buttons.buttons_to_use.RIGHT)
+		{
+			control = App->input->scancode;
+			button->has_been_clicked = false;
+			button->Select(SELECTED);
+			isWaittingButt = false;
+			label->SetText(SDL_GetScancodeName(App->input->scancode));
+		}
+		else
+			isWaittingButt = true;
+	}
+	else 
+	{
+		if (isWaittingButt && App->input->scancode != SDL_SCANCODE_UNKNOWN
+			&& App->input->scancode != App->input->gamepad.A
+			&& App->input->scancode != App->input->gamepad.B
+			&& App->input->scancode != App->input->gamepad.START)
+		{
+			control = App->input->scancode;
+			button->has_been_clicked = false;
+			button->Select(SELECTED);
+			isWaittingButt = false;
+			label->SetText(SDL_GetScancodeName(App->input->scancode));
+		}
+		else
+			isWaittingButt = true;
+	}
+	
+
+	
 }
 
 	
